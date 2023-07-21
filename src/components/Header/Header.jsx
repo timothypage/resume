473b9b6e48550e0eb48775d47a465070cfaa334c
Tim Zwolak <timothypage@gmail.com>
2023-07-21 11:07:03 -0600

    finish up header layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,50 @@
+import { TbBrandSolidjs } from "solid-icons/tb"
+import  { 
+  RiBusinessMailLine,
+  RiMapMapPinLine, 
+  RiDevicePhoneFill,
+  RiLogosGithubFill
+} from "solid-icons/ri"
+
 import styles from "./Header.module.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div class={styles.header}>
+      <div class={styles.icon}>
+        <p>tz</p>
+      </div>
+      <div class={styles.title}>
+        <h1>Tim Zwolak</h1>
+      </div>
+      <div class={styles.contact}>
+        <ul class={styles.contactList}>
+          <li>
+            <span><RiBusinessMailLine /></span>
+            <a href="mailto:***REMOVED***">***REMOVED***</a>
+          </li>
+          <li>
+            <span><RiDevicePhoneFill /></span>
+            <a 
+              href="tel:+1***REMOVED***" 
+              aria-label="***REMOVED***"
+            >***REMOVED***</a>
+          </li>
+          <li>
+            <span><RiMapMapPinLine /></span>
+            <span class={styles.address} aria-label="home address">
+              nope
+            </span>
+          </li>
+          <li>
+            <span><RiLogosGithubFill /></span>
+            <a href="https://github.com/timothypage" area-label="github user profile">
+              timothypage
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
