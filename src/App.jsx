77a00a97af7f4b52e52589a728e82a23c4d8e756
Tim Zwolak <timothypage@gmail.com>
2023-07-21 09:49:51 -0600

    add some global styles and MaxWidthWrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import MaxWidthWrapper from '/src/components/MaxWidthWrapper'
 import Header from '/src/components/Header'
 
-function App() {
-
+function App () {
   return (
-    <>
+    <MaxWidthWrapper>
       <h1>Hello World</h1>
       <Header />
-    </>
+    </MaxWidthWrapper>
   )
 }
 
