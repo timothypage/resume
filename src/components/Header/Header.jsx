3bf967378d17407dda98d718fd9781cbca1a422f
Tim Zwolak <timothypage@gmail.com>
2023-07-30 21:53:28 -0600

    show email and phone from query params

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
-import { TbBrandSolidjs } from "solid-icons/tb"
-import  { 
+import { TbBrandSolidjs } from 'solid-icons/tb'
+import {
   RiBusinessMailLine,
-  RiMapMapPinLine, 
+  RiMapMapPinLine,
   RiDevicePhoneFill,
   RiLogosGithubFill
-} from "solid-icons/ri"
+} from 'solid-icons/ri'
 
-import styles from "./Header.module.css"
+import styles from './Header.module.css'
 
 const Header = () => {
+  const phone = new URLSearchParams(window.location.search).get('phone')
+  const email = new URLSearchParams(window.location.search).get('email')
+
   return (
     <div class={styles.header}>
       <div class={styles.tzIcon}>
@@ -19,26 +22,45 @@ const Header = () => {
       </div>
       <div class={styles.contact}>
         <ul class={styles.contactList}>
+          <Show when={email}>
+            <li>
+              <span class={styles.icon}>
+                <RiBusinessMailLine />
+              </span>
+              <a href={`mailto:${email}`}>{email}</a>
+            </li>
+          </Show>
+
+          <Show when={phone}>
+            <li>
+              <span>
+                <RiDevicePhoneFill />
+              </span>
+              <a
+                href={`tel:+1${phone}`}
+                aria-label={`cell phone ${phone.split('').join(' ')}`}
+              >
+                {phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3')}
+              </a>
+            </li>
+          </Show>
+
           <li>
-            <span class={styles.icon}><RiBusinessMailLine /></span>
-            <a href="mailto:***REMOVED***">***REMOVED***</a>
-          </li>
-          <li>
-            <span><RiDevicePhoneFill /></span>
-            <a 
-              href="tel:+1***REMOVED***" 
-              aria-label="***REMOVED***"
-            >***REMOVED***</a>
-          </li>
-          <li>
-            <span><RiMapMapPinLine /></span>
-            <span class={styles.address} aria-label="home address">
+            <span>
+              <RiMapMapPinLine />
+            </span>
+            <span class={styles.address} aria-label='home address'>
               Littleton, CO
             </span>
           </li>
           <li>
-            <span><RiLogosGithubFill /></span>
-            <a href="https://github.com/timothypage" area-label="github user profile">
+            <span>
+              <RiLogosGithubFill />
+            </span>
+            <a
+              href='https://github.com/timothypage'
+              area-label='github user profile'
+            >
               timothypage
             </a>
           </li>
