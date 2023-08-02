d739d2411fdf7289034a5c2ef9a735b4ce653a7f
Tim Zwolak <timothypage@gmail.com>
2023-08-01 19:10:16 -0600

    add sub resource integrity because why not

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import sri from '@small-tech/vite-plugin-sri'
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), sri()],
 
   build: {
-    sourcemap: true
+    // sourcemap: true
   },
 
   css: {
