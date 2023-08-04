a70948dfeddb0a93497196239cfbf20dbd61040b
Tim Zwolak <timothypage@gmail.com>
2023-08-04 09:58:52 -0600

    turn off nginx gzip in favor of gzipping static assets at build time
    and added a preload directive for the fonts so they aren't dependent on
    loading the css first - per pagespeed insights

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import sri from '@small-tech/vite-plugin-sri'
 
+import { gzipAsync } from '@gfx/zopfli'
+import gzipPlugin from 'rollup-plugin-gzip'
+
 export default defineConfig({
-  plugins: [solid(), sri()],
+  plugins: [
+    solid(), 
+    sri(),
+    gzipPlugin({
+      customCompression: content =>
+        gzipAsync(Buffer.from(content), { numiterations: 15 }),
+    })
+  ],
 
   build: {
     // sourcemap: true
